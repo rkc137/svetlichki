db35aa5484bf480c2d7c214e2b78f67a861485ea
rkc137 <hlopov100@gmail.com>
2026-06-19 05:00:40 +0300

    res cpp

---
+++ src/res.cpp
@@ -0,0 +1,42 @@
+#include "res.hpp"
+
+float res::get_tgt_sprite_size() const
+{
+    return tgt_sprite_size;
+}
+
+void res::load(sf::Vector2u window_size, float ratio)
+{
+    auto [w, h] = window_size;
+    tgt_sprite_size = std::max(w, h) * ratio;
+
+    std::ifstream ifs(folder / "config.json");
+    if(!ifs.is_open())
+        throw std::runtime_error("failed to open config");
+    auto config = json::parse(ifs);
+
+    for(auto &image : config["images"])
+    {
+        Texture texture;
+        if(!texture.loadFromFile(folder / image["texture_name"].get<std::string>()))
+            throw std::runtime_error("failed to open texture");
+
+        
+        if(auto size = image["frame_size"]; size.size() != 2)
+            throw std::runtime_error("wrong amount of arguments in frame_size");
+        else
+            texture.frame_size = {
+                size[0].get<int>(),
+                size[1].get<int>()
+            };
+        
+        texture.animations_lengths = image["anim_lens"].get<std::vector<int>>();
+        
+        bool is_smooth = true;
+        if(auto it = image.find("is_pixel"); it != image.end())
+            is_smooth = it->get<bool>();
+        texture.setSmooth(is_smooth);
+        
+        textures.push_back(std::move(texture));
+    }
+}
--- src/res.hpp
+++ src/res.hpp
@@ -26,44 +26,6 @@ public:
     };
     inline static std::vector<Texture> textures;
 
-    static float get_tgt_sprite_size()
-    {
-        return tgt_sprite_size;
-    }
-
-    static void load(sf::Vector2u window_size, float ratio = .1f)
-    {
-        auto [w, h] = window_size;
-        tgt_sprite_size = std::max(w, h) * ratio;
-
-        std::ifstream ifs(folder / "config.json");
-        if(!ifs.is_open())
-            throw std::runtime_error("failed to open config");
-        auto config = json::parse(ifs);
-
-        for(auto &image : config["images"])
-        {
-            Texture texture;
-            if(!texture.loadFromFile(folder / image["texture_name"].get<std::string>()))
-                throw std::runtime_error("failed to open texture");
-
-            
-            if(auto size = image["frame_size"]; size.size() != 2)
-                throw std::runtime_error("wrong amount of arguments in frame_size");
-            else
-                texture.frame_size = {
-                    size[0].get<int>(),
-                    size[1].get<int>()
-                };
-            
-            texture.animations_lengths = image["anim_lens"].get<std::vector<int>>();
-            
-            bool is_smooth = true;
-            if(auto it = image.find("is_pixel"); it != image.end())
-                is_smooth = it->get<bool>();
-            texture.setSmooth(is_smooth);
-            
-            textures.push_back(std::move(texture));
-        }
-    }
+    static float get_tgt_sprite_size() const;
+    static void load(sf::Vector2u window_size, float ratio = .1f);
 };
